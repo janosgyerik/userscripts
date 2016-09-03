817819e1db62403ffbadaf8e0cd96af6e9575f13
Janos Gyerik <janos@kronos>
2016-09-03 23:15:33 +0200

    Fix page filter

--- se-hide-earned-badges.user.js
+++ se-hide-earned-badges.user.js
@@ -3,22 +3,20 @@
 // @author Janos Gyerik
 // @description Show a link in Help Center > Badges to hide earned badges.
 // @namespace https://github.com/janosgyerik/userscripts
-// @match *://*.stackexchange.com/review*
-// @match *://*.stackoverflow.com/review*
-// @match *://*.superuser.com/review*
-// @match *://*.serverfault.com/review*
-// @match *://*.askubuntu.com/review*
-// @match *://*.stackapps.com/review*
-// @match *://*.mathoverflow.net/review*
+// @match *://*.stackexchange.com/help/badges*
+// @match *://*.stackoverflow.com/help/badges*
+// @match *://*.superuser.com/help/badges*
+// @match *://*.serverfault.com/help/badges*
+// @match *://*.askubuntu.com/help/badges*
+// @match *://*.stackapps.com/help/badges*
+// @match *://*.mathoverflow.net/help/badges*
 // ==/UserScript==
 
-if (window.location.href.indexOf('/help/badges') !== -1) {
-    var hideEarnedLink = $('<a>hide earned</a>');
+var hideEarnedLink = $('<a>hide earned</a>');
 
-    hideEarnedLink.click(function() {
-        $('.badge-earned-check').parents('.badge-row').remove();
-        $('.badge-hierarchy').filter(function() { return $(this).text().trim().length == 0; }).hide();
-    });
+hideEarnedLink.click(function() {
+    $('.badge-earned-check').parents('.badge-row').remove();
+    $('.badge-hierarchy').filter(function() { return $(this).text().trim().length == 0; }).hide();
+});
 
-    $('#h-badges').append(' (').append(hideEarnedLink).append(')');
-}
+$('#h-badges').append(' (').append(hideEarnedLink).append(')');
