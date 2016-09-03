1f06b16a01a636459bfe6e4424f3475b676048a1
Janos Gyerik <janos@kronos>
2016-09-03 23:03:33 +0200

    Add userscript to hide earned badges on stack exchange

---
+++ se-hide-earned-badges.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name Hide Earned Badges
+// @author Janos Gyerik
+// @description Show a link in Help Center > Badges to hide earned badges.
+// @namespace https://github.com/janosgyerik/userscripts
+// @match *://*.stackexchange.com/review*
+// @match *://*.stackoverflow.com/review*
+// @match *://*.superuser.com/review*
+// @match *://*.serverfault.com/review*
+// @match *://*.askubuntu.com/review*
+// @match *://*.stackapps.com/review*
+// @match *://*.mathoverflow.net/review*
+// ==/UserScript==
+
+if (window.location.href.indexOf('/help/badges') !== -1) {
+    var hideEarnedLink = $('<a>(hide earned)</a>');
+
+    hideEarnedLink.click(function() {
+        $('.badge-earned-check').parents('.badge-row').remove();
+        $('.badge-hierarchy').filter(function() { return $(this).text().trim().length == 0; }).hide();
+    });
+
+    $('#h-badges').append(hideEarnedLink);
+}
