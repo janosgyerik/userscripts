c69967bcfa997665ea7ecb1e2b031aeac422731c
Janos Gyerik <janos.gyerik@gmail.com>
2016-09-22 21:22:49 +0200

    Update leetcode userscript to follow updated site

--- leetcode-filter-problems.user.js
+++ leetcode-filter-problems.user.js
@@ -1,22 +1,20 @@
 // ==UserScript==
 // @name         Filter Problems on Leetcode
 // @namespace    http://www.janosgyerik.com/
-// @version      0.1
-// @description  enter something useful
+// @version      0.2
+// @description  Add buttons on the problem listing page to apply various filters
 // @author       janos
 // @match        https://leetcode.com/problemset/algorithms/
 // @grant        none
 // ==/UserScript==
 
 $(document).ready(function() {
-    // hide locked
-    $('.fa-lock').parent('td').parent('tr').hide()
-    
     var toolbar = $('<div/>');
     toolbar.append(
+        $('<button/>').append('Hide Locked').click(function() { $('.fa-lock').parent('div').parent('td').parent('tr').hide(); }),
         $('<button/>').append('Hide Easy').click(function() { $('td:contains("Easy")').parent('tr').hide(); }),
         $('<button/>').append('Hide Medium').click(function() { $('td:contains("Medium")').parent('tr').hide(); }),
-        $('<button/>').append('Hide Completed').click(function() { $('td span.ac').parent('td').parent('tr').hide(); })
+        $('<button/>').append('Hide Completed').click(function() { $('.fa-check').parent('td').parent('tr').hide(); })
     );
-    $('#brief_stats').prepend(toolbar);
+    $('#question-app').prepend(toolbar);
 });
